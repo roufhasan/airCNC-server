81779ab00d85032ef8d090b61cb36a2446145d36
roufhasane <roufhasan5@gmail.comm>
2023-09-05 22:17:34 +0600

    single user api created using email

--- index.js
+++ index.js
@@ -44,6 +44,15 @@ async function run() {
       res.send(result);
     });
 
+    // Get user
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await usersCollection.findOne(query);
+      console.log(result);
+      res.send(result);
+    });
+
     // Get all rooms
     app.get("/rooms", async (req, res) => {
       const result = await roomsCollection.find().toArray();
